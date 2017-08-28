a25b543e07b164e0147be180f5d64bda81eedc3a
bkis <kiss@mailbox.org>
2017-08-28 17:39:32 +0200

    init

---
+++ src/main/java/de/unikoeln/vedaweb/vedaweb/VedawebApplication.java
@@ -0,0 +1,12 @@
+package de.unikoeln.vedaweb.vedaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedawebApplication.class, args);
+	}
+}
