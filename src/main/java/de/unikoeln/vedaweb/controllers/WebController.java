1ed5cd28043cd89f5ed8816f21f6e96c5a3c5f65
bkis <kiss@mailbox.org>
2017-12-15 16:37:10 +0100

    split frontend into thymeleaf fragments, controller

---
+++ src/main/java/de/unikoeln/vedaweb/controllers/WebController.java
@@ -0,0 +1,18 @@
+package de.unikoeln.vedaweb.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	
+	@RequestMapping("/")
+    public String app(Model model, HttpServletRequest request) {
+		System.out.println("[INFO] called '/', serving template 'app'.");
+    	return "app";
+    }
+
+}
