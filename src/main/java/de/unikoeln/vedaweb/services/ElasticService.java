f389e86d73f848dc35f3fc4502964994b8ba7b65
mumpitz <kiss@mailbox.org>
2018-02-06 16:52:37 +0100

    changed property resource handling

--- src/main/java/de/unikoeln/vedaweb/services/ElasticService.java
+++ src/main/java/de/unikoeln/vedaweb/services/ElasticService.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
-@PropertySource(ignoreResourceNotFound = true, value = "classpath:es.properties")
+@PropertySource(value = "classpath:es.properties")
 public class ElasticService {
 	
 	// es connection config
