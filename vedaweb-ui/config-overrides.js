3e34f4fa8c76ea0d3646ee31fb501c74c213d827
Mumpitz Firlefanz <kiss@mailbox.org>
2018-03-12 18:48:46 +0100

    installed react-app-rewire-less and changed primary theme color

--- vedaweb-ui/config-overrides.js
+++ vedaweb-ui/config-overrides.js
@@ -1,6 +1,10 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);
+    config = rewireLess.withLoaderOptions({
+         modifyVars: { "@primary-color": "#443" },
+       })(config, env);
     return config;
 };
