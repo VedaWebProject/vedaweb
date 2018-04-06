bb3684ec0ef447d5d00cc12f064ceb96bbcf0681
Mumpitz Firlefanz <kiss@mailbox.org>
2018-04-06 16:12:49 +0200

    use external .less file for overriding theme variables

--- vedaweb-ui/config-overrides.js
+++ vedaweb-ui/config-overrides.js
@@ -1,13 +1,15 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
+const path = require('path');
+const fs  = require('fs');
+const lessToJs = require('less-vars-to-js');
+const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, './ant-custom-vars.less'), 'utf8'));
+
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);
     config = rewireLess.withLoaderOptions({
-        modifyVars: {
-             "@primary-color": "#443",
-             "@animation-duration-slow": ".2s"
-        }
+        modifyVars: themeVariables
     })(config, env);
 
     return config;
