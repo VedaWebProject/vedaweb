3e6f4989b27b13efcf02f4228c2d8a128b040f6d
Mumpitz Firlefanz <kiss@mailbox.org>
2018-04-09 17:08:50 +0200

    testing local font options

--- vedaweb-ui/config-overrides.js
+++ vedaweb-ui/config-overrides.js
@@ -7,6 +7,7 @@ const lessToJs = require('less-vars-to-js');
 const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, './ant-custom-vars.less'), 'utf8'));
 
 module.exports = function override(config, env) {
+    //themeVariables["@icon-url"] = "'/fonts/iconfont'";
     config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);
     config = rewireLess.withLoaderOptions({
         modifyVars: themeVariables
