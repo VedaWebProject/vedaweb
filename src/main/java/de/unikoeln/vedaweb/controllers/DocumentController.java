607a234682d9ec1bfb7788a464c671c930f2c18f
Mumpitz Firlefanz <kiss@mailbox.org>
2018-04-26 17:28:44 +0200

    removed unused imports

--- src/main/java/de/unikoeln/vedaweb/controllers/DocumentController.java
+++ src/main/java/de/unikoeln/vedaweb/controllers/DocumentController.java
@@ -3,10 +3,7 @@ package de.unikoeln.vedaweb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-
 import de.unikoeln.vedaweb.data.VerseRepository;
 import de.unikoeln.vedaweb.services.MappingService;
 
