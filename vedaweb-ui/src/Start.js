77028cdd324fec91719cc39223fda6c15e69c8b3
bkis <kiss@mailbox.org>
2018-08-03 14:09:05 +0200

    WIP: text columns in start view for presentations purposes

--- vedaweb-ui/src/Start.js
+++ vedaweb-ui/src/Start.js
@@ -11,8 +11,11 @@ class ContentView extends Component {
                 <Col span={24}>
                     <div className="card">
                         <h4>VedaWeb</h4>
-                        (Juli 2017-Juni 2020)<br/><br/>
+                        <span>(Juli 2017-Juni 2020)</span>
+                        <div className="start">
+                        <br/>
                         Das Projekt hat zum Ziel, eine webbasierte, frei zugängliche Plattform für die sprachwissenschaftliche Erforschung altindischer Texte aufzubauen. Das altindische Textkorpus soll digital einsehbar, morphologisch und metrisch annotiert sowie nach lexikographischen und korpuslinguistischen Kriterien durchsuchbar zur Verfügung gestellt werden. Als Pilottext wird der Rigveda eingespeist werden, der mit den Wörterbüchern Cologne Digital Sanskrit Dictionaries verknüpft werden soll. Die morphologische Annotation dieses Textes wurde an der Universität Zürich (UZH) durchgeführt und dem Projekt zur Verfügung gestellt. In Zukunft wird über die URL vedaweb.uni-koeln.de die Vedaweb-Anwendung erreichbar sein.
+                        </div>
                     </div>
                 </Col>
             </Row>
