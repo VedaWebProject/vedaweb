5866ae94b104ad7fd4196e35c2a0cec24a771f5d
bkis <kiss@mailbox.org>
2018-08-29 14:45:34 +0200

    startpage prep

--- vedaweb-frontend/src/Start.js
+++ vedaweb-frontend/src/Start.js
@@ -10,11 +10,10 @@ class ContentView extends Component {
             <Row id="content-view" className="page-content">
                 <Col span={24}>
                     <div className="card">
-                        <h4>VedaWeb</h4>
-                        <span>(Juli 2017-Juni 2020)</span>
+                        <h4>VedaWeb beta</h4>
+                        <div>Projektzeitraum: Juli 2017 - Juni 2020<br/><br/></div>
                         <div className="start">
-                        <br/>
-                        Das Projekt hat zum Ziel, eine webbasierte, frei zugängliche Plattform für die sprachwissenschaftliche Erforschung altindischer Texte aufzubauen. Das altindische Textkorpus soll digital einsehbar, morphologisch und metrisch annotiert sowie nach lexikographischen und korpuslinguistischen Kriterien durchsuchbar zur Verfügung gestellt werden. Als Pilottext wird der Rigveda eingespeist werden, der mit den Wörterbüchern Cologne Digital Sanskrit Dictionaries verknüpft werden soll. Die morphologische Annotation dieses Textes wurde an der Universität Zürich (UZH) durchgeführt und dem Projekt zur Verfügung gestellt. In Zukunft wird über die URL vedaweb.uni-koeln.de die Vedaweb-Anwendung erreichbar sein.
+                            Das Projekt hat zum Ziel, eine webbasierte, frei zugängliche Plattform für die sprachwissenschaftliche Erforschung altindischer Texte aufzubauen. Das altindische Textkorpus soll digital einsehbar, morphologisch und metrisch annotiert sowie nach lexikographischen und korpuslinguistischen Kriterien durchsuchbar zur Verfügung gestellt werden. Als Pilottext wird der Rigveda eingespeist werden, der mit den Wörterbüchern Cologne Digital Sanskrit Dictionaries verknüpft werden soll. Die morphologische Annotation dieses Textes wurde an der Universität Zürich (UZH) durchgeführt und dem Projekt zur Verfügung gestellt. In Zukunft wird über die URL vedaweb.uni-koeln.de die Vedaweb-Anwendung erreichbar sein.
                         </div>
                     </div>
                 </Col>
