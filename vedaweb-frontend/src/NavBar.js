2fac7b6a2471dc9d7ded799a1fc040a341c86852
bkis <kiss@mailbox.org>
2018-08-29 15:37:14 +0200

    cleanup

--- vedaweb-frontend/src/NavBar.js
+++ vedaweb-frontend/src/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col, Icon, Menu } from 'antd';
+import { Row, Icon, Menu } from 'antd';
 
 import SearchSmart from "./SearchSmart";
 import logo from "./img/logo.png";
@@ -32,53 +32,45 @@ class NavBar extends Component {
             id="navbar"
             className="box-shadow">
 
-                {/* <Col
-                span={4}
-                className="content-left"> */}
-                    <NavLink to={"/home"} className="v-middle">
-                        <img src={logo} className="navbar-logo" alt="" />
-                        <div className="navbar-app-title">
-                            <span className="bold">VedaWeb</span><br/>
-                            Rigveda online
-                        </div>
-                    </NavLink>
-                {/* </Col> */}
+                <NavLink to={"/home"} className="v-middle">
+                    <img src={logo} className="navbar-logo" alt="" />
+                    <div className="navbar-app-title">
+                        <span className="bold">VedaWeb</span><br/>
+                        Rigveda online
+                    </div>
+                </NavLink>
 
-                {/* <Col span={8}> */}
                 <div className="flex-grow-1">
                     <SearchSmart />
                 </div>
-                {/* </Col> */}
 
-                {/* <Col span={12}> */}
-                    <div className="flex-grow-2">
-                        <Menu
-                        selectedKeys={[]}
-                        mode="horizontal"
-                        style={menuStyle}>
-                            
-                            <Menu.Item key="search">
-                                <NavLink to={"/search"}>
-                                    {/* <Icon type="search"/> */}
-                                    Advanced Search
-                                </NavLink>
-                            </Menu.Item>
+                <div className="flex-grow-2">
+                    <Menu
+                    selectedKeys={[]}
+                    mode="horizontal"
+                    style={menuStyle}>
+                        
+                        <Menu.Item key="search">
+                            <NavLink to={"/search"}>
+                                {/* <Icon type="search"/> */}
+                                Advanced Search
+                            </NavLink>
+                        </Menu.Item>
 
-                            <SubMenu
-                            title={<span><Icon type="book"/>Browse Rigveda</span>}
-                            className="right">
-                                {searchMetaStore.scopeDataRaw.map((hymns, i) => (
-                                    <Menu.Item key={'view_' + i}>
-                                        <NavLink to={"/view/id/" + (i+1) + ".1.1"}>
-                                            Book {('0' + (i+1)).slice(-2)} ({hymns} Hymns)
-                                        </NavLink>
-                                    </Menu.Item>
-                                ))}
-                            </SubMenu>
+                        <SubMenu
+                        title={<span><Icon type="book"/>Browse Rigveda</span>}
+                        className="right">
+                            {searchMetaStore.scopeDataRaw.map((hymns, i) => (
+                                <Menu.Item key={'view_' + i}>
+                                    <NavLink to={"/view/id/" + (i+1) + ".1.1"}>
+                                        Book {('0' + (i+1)).slice(-2)} ({hymns} Hymns)
+                                    </NavLink>
+                                </Menu.Item>
+                            ))}
+                        </SubMenu>
 
-                        </Menu>
-                    </div>
-                {/* </Col> */}
+                    </Menu>
+                </div>
 
             </Row>
             
