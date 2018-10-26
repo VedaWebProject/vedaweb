7d41bddef732de50a5f53618a84574bb58f225c6
bkis <kiss@mailbox.org>
2018-10-26 11:05:25 +0200

    import process optimizations

--- vedaweb-backend/src/main/java/de/unikoeln/vedaweb/controllers/SystemController.java
+++ vedaweb-backend/src/main/java/de/unikoeln/vedaweb/controllers/SystemController.java
@@ -1,6 +1,8 @@
 package de.unikoeln.vedaweb.controllers;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,12 +42,15 @@ public class SystemController {
 		}
     }
 	
-	
-	@RequestMapping(value = "/data/import/{dryRun}", produces = {"application/json"})
-    public String importData(@PathVariable("dryRun") String dryRun) {
-		int docCount = dataImportService.importXMLData(DataImportService.LOCAL_XML, dryRun.equals("true"));
-    	return "{\"verses\":" + docCount + "}";
+	@GetMapping(value = {"/import/{dryRun}", "/import"}, produces = {"application/json"})
+    public String importData(@PathVariable(name = "dryRun", required = false) String dryRun) {
+		boolean dry = dryRun != null;
+		int docCount = dataImportService.importXMLData(DataImportService.LOCAL_XML, dry);
+		JSONObject response = new JSONObject();
+		response.put("dryRun", dry);
+		response.put("importedDocsCount", docCount);
+		if (!dry) response.put("indexActions", indexService.rebuildIndex());
+		return response.toString();
     }
-	
 	
 }
