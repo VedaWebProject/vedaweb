e3f3f40b9a7e99c4f6b11e835a249cf01c4a1ec3
bkis <kiss@mailbox.org>
2018-11-30 14:28:08 +0100

    accent detection fix/improvement

--- vedaweb-backend/src/main/java/de/unikoeln/vedaweb/util/StringUtils.java
+++ vedaweb-backend/src/main/java/de/unikoeln/vedaweb/util/StringUtils.java
@@ -5,17 +5,7 @@ import java.text.Normalizer.Form;
 
 public class StringUtils {
 	
-	
-	public static String normalizeNFC(String s){
-		return s == null ? "" :
-			Normalizer.normalize(s, Form.NFC);
-	}
-	
-	
-	public static String normalizeNFD(String s){
-		return s == null ? "" :
-			Normalizer.normalize(s, Form.NFD);
-	}
+	private static final String REGEX_VOWEL_W_ACCENT = "([aeiouAEIOU][^aeiouAEIOU]*?)[\\u0301\\u0300]";
 	
 	
 	public static int normalizeIndex(int index, int docCount){
@@ -29,30 +19,28 @@ public class StringUtils {
 	
 	public static boolean containsAccents(String text) {
 		if (text == null) return false;
-		text = normalizeNFC(text);
-		
-		for (int i = 0; i < text.length(); i++) {
-			String c = normalizeNFD(text.substring(i, i + 1));
-			if (c.matches("[aeiouAEIOU].*") && c.matches(".*(\\u0301|\\u0300).*")) {
-				return true;
-			}
-		}
-	    return false;
+	    return normalizeNFD(text).matches(".*" + REGEX_VOWEL_W_ACCENT + ".*");
 	}
 	
 	
 	public static String removeVowelAccents(String text) {
 		if (text == null) return text;
-		text = normalizeNFC(text);
-		StringBuilder out = new StringBuilder();
-		
-		for (int i = 0; i < text.length(); i++) {
-			String c = normalizeNFD(text.substring(i, i + 1));
-			if (c.matches("[aeiouAEIOU].*")) c = c.replaceAll("[\\u0301\\u0300]", "");
-			out.append(normalizeNFC(c));
-		}
-	    
-		return out.toString();
+		return normalizeNFC(
+			normalizeNFD(text)
+				.replaceAll(REGEX_VOWEL_W_ACCENT, "$1")
+		);
+	}
+	
+	
+	public static String normalizeNFC(String s){
+		return s == null ? "" :
+			Normalizer.normalize(s, Form.NFC);
+	}
+	
+	
+	public static String normalizeNFD(String s){
+		return s == null ? "" :
+			Normalizer.normalize(s, Form.NFD);
 	}
 	
 
