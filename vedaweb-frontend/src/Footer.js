10834c112f8bfbc9a26e4379eb5c16b4a5704bbf
bkis <kiss@mailbox.org>
2018-12-14 18:54:36 +0100

    extended logo park, closes #146

--- vedaweb-frontend/src/Footer.js
+++ vedaweb-frontend/src/Footer.js
@@ -7,6 +7,11 @@ import logoCSALTAPI from "./img/logo_csalt_api.png";
 import logoCSL from "./img/logo_csl.png";
 import logoTCSL from "./img/logo_tcsl.png";
 
+import logoDFG from "./img/logo_dfg.gif";
+import logoDCH from "./img/logo_dch.jpg";
+import logoIDH from "./img/logo_idh.png";
+import logoCCEH from "./img/logo_cceh.png";
+
 import { Link } from 'react-router-dom';
 
 import "./css/Footer.css";
@@ -17,57 +22,78 @@ class Footer extends Component {
         return (
 
             <div id="footer">
-            
+
                 <Row
                 type="flex"
                 justify="center"
-                align="top">
+                align="middle">
+
+                    <Col span={3} className="footer-box">
+                        <a href="http://www.dfg.de/en" className="bw" target="_blank" rel="noopener noreferrer">
+                            <img src={logoDFG} alt="Gefördert durch die DFG"/>
+                        </a>
+                    </Col>
+
+                    <Col span={3} className="footer-box">
+                        <a href="http://dh.uni-koeln.de/" className="bw" target="_blank" rel="noopener noreferrer">
+                            <img src={logoIDH} alt="IDH"/>
+                        </a>
+                    </Col>
+
+                    <Col span={3} className="footer-box">
+                        <a href="http://cceh.uni-koeln.de/" className="bw" target="_blank" rel="noopener noreferrer">
+                            <img src={logoCCEH} alt="CCEH"/>
+                        </a>
+                    </Col>
+
+                    <Col span={3} className="footer-box">
+                        <a href="http://dch.phil-fak.uni-koeln.de/" className="bw" target="_blank" rel="noopener noreferrer">
+                            <img src={logoDCH} alt="DCH"/>
+                        </a>
+                    </Col>
+                </Row>
+
+                <Row
+                type="flex"
+                justify="center"
+                align="top"
+                className="bottom-gap"
+                style={{marginTop:'3rem'}}>
 
                     <Col span={3} className="footer-box">
                         <a href="http://c-salt.uni-koeln.de" target="_blank" rel="noopener noreferrer">
-                            <div className="footer-box">
-                                <img src={logoCSALT} alt="" /><br/><br/>
-                                C-SALT
-                            </div>
+                            <img className="bottom-gap" src={logoCSALT} alt="" /><br/>
+                            C-SALT
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="http://sanskrit-lexicon.uni-koeln.de" target="_blank" rel="noopener noreferrer">
-                            <div className="footer-box">
-                                <img src={logoCSL} alt="" /><br/><br/>
-                                Cologne Sanskrit<br/>Lexicon
-                            </div>
+                            <img className="bottom-gap" src={logoCSL} alt="" /><br/>
+                            Cologne Sanskrit<br/>Lexicon
                         </a>
                     </Col>
 
-                    <Col span={3}>
+                    <Col span={3} className="footer-box">
                         <a href="http://sanskrit-lexicon.uni-koeln.de/csl" target="_blank" rel="noopener noreferrer">
-                            <div className="footer-box">
-                                <img src={logoTCSL} alt="" /><br/><br/>
-                                TEI Cologne Sanskrit<br/>Lexicon
-                            </div>
+                            <img className="bottom-gap" src={logoTCSL} alt="" /><br/>
+                            TEI Cologne Sanskrit<br/>Lexicon
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="http://cpd.uni-koeln.de" target="_blank" rel="noopener noreferrer">
-                            <div className="footer-box">
-                                <img src={logoCPDO} alt="" /><br/><br/>
-                                Critical Pāli<br/>Dictionary Online
-                            </div>
+                            <img className="bottom-gap" src={logoCPDO} alt="" /><br/>
+                            Critical Pāli<br/>Dictionary Online
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="https://api.c-salt.uni-koeln.de/" target="_blank" rel="noopener noreferrer">
-                            <div className="footer-box">
-                                <img src={logoCSALTAPI} alt="" /><br/><br/>
-                                C-SALT APIs<br/>for Dictionaries
-                            </div>
+                            <img className="bottom-gap" src={logoCSALTAPI} alt="" /><br/>
+                            C-SALT APIs<br/>for Dictionaries
                         </a>
                     </Col>
-
                 </Row>
 
                 <Row className="top-gap">
@@ -82,7 +108,7 @@ class Footer extends Component {
 
                 <Row>
                     <Col span={24} className="content-center">
-                        <p className="top-gap content-center font-small light-grey">
+                        <p className="content-center font-small light-grey">
                             VedaWeb, ed. by Daniel Kölligan and Uta Reinöhl,<br/>
                             in collaboration with Jakob Halfmann, Börge Kiss, Natalie Korobzow,<br/>Francisco Mondaca, Claes Neuefeind, Felix Rau and Patrick Sahle,<br/>
                             with material provided by Paul Widmer et al. Cologne: University of Cologne 2018.
