75f21dc4c68fba661dedd9c97046ad262ceeec40
bkis <kiss@mailbox.org>
2019-02-25 16:44:03 +0100

    updated deps, fixed versioning error, fixed less overrides

--- vedaweb-frontend/config-overrides.js
+++ vedaweb-frontend/config-overrides.js
@@ -1,18 +1,25 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
-const path = require('path');
-const fs  = require('fs');
-const lessToJs = require('less-vars-to-js');
-const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, './ant-custom-vars.less'), 'utf8'));
+module.exports = override(
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: {
+            '@primary-color': '#931111',
+            '@font-size-base': '16px',
+            '@font-size-sm' : '14px',
+            '@animation-duration-slow': '.2s',
+            '@border-radius-base': '3px',
+            '@border-radius-sm': '2px',
+            '@font-family' : 'Assistant, sans-serif',
+            '@font-family-no-number': 'Assistant, sans-serif',
+            '@border-color-base': '#b4b1ae',
+            '@table-row-hover-bg': 'none'
+        },
+    }),
+  );
 
-module.exports = function override(config, env) {
-    //themeVariables["@icon-url"] = "'/fonts/iconfont'";
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);
-    config = rewireLess.withLoaderOptions({
-        modifyVars: themeVariables,
-        javascriptEnabled: true
-    })(config, env);
-
-    return config;
-};
