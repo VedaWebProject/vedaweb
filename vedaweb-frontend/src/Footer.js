3185bc8428c5095c12ffbc6062ff65a455e0eaba
bkis <kiss@mailbox.org>
2019-03-29 15:59:16 +0100

    footer cosmetics

--- vedaweb-frontend/src/Footer.js
+++ vedaweb-frontend/src/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col } from 'antd';
+import { Row, Col, Icon } from 'antd';
 
 import logoCPDO from "./img/logo_cpdo.png";
 import logoCSALT from "./img/logo_csalt.png";
@@ -28,56 +28,61 @@ class Footer extends Component {
 
                 <Row
                 type="flex"
-                justify="center"
-                align="middle">
+                justify="space-around"
+                align="top">
 
                     <Col span={3} className="footer-box">
                         <a href="http://ifl.phil-fak.uni-koeln.de/asw2.html"
-                        title="Allgemeine Sprachwissenschaft (IfL), University of Cologne"
+                        title="Allgemeine Sprachwissenschaft (IfL), Universität zu Köln"
                         className="bw"
                         target="_blank"
                         rel="noopener noreferrer">
-                            <img src={logoASW} alt="Allgemeine Sprachwissenschaft (IfL), University of Cologne"/>
+                            <img src={logoASW} alt="Allgemeine Sprachwissenschaft (IfL), Universität zu Köln"/><br/>
+                            Allgemeine<br/>Sprachwissenschaft (IfL),<br/>Universität zu Köln
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="http://ifl.phil-fak.uni-koeln.de/hvs2.html"
-                        title="Historisch-Vergleichende Sprachwissenschaft (IfL), University of Cologne"
+                        title="Historisch-Vergleichende Sprachwissenschaft (IfL), Universität zu Köln"
                         className="bw"
                         target="_blank"
                         rel="noopener noreferrer">
-                            <img src={logoHVS} alt="Historisch-Vergleichende Sprachwissenschaft (IfL), University of Cologne"/>
+                            <img src={logoHVS} alt="Historisch-Vergleichende Sprachwissenschaft (IfL), Universität zu Köln"/><br/>
+                            Historisch-Vergleichende<br/>Sprachwissenschaft (IfL),<br/>Universität zu Köln
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="http://dh.uni-koeln.de/"
-                        title="Institut für Digital Humanities, University of Cologne"
+                        title="Institut für Digital Humanities, Universität zu Köln"
                         className="bw"
                         target="_blank"
                         rel="noopener noreferrer">
-                            <img src={logoIDH} alt="IDH"/>
+                            <img src={logoIDH} alt="IDH"/><br/>
+                            Institut für<br/>Digital Humanities,<br/>Universität zu Köln
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="http://cceh.uni-koeln.de/"
-                        title="Cologne Center for eHumanities, University of Cologne"
+                        title="Cologne Center for eHumanities, Universität zu Köln"
                         className="bw"
                         target="_blank"
                         rel="noopener noreferrer">
-                            <img src={logoCCEH} alt="CCEH"/>
+                            <img src={logoCCEH} alt="CCEH"/><br/>
+                            Cologne Center<br/>for eHumanities,<br/>Universität zu Köln
                         </a>
                     </Col>
 
                     <Col span={3} className="footer-box">
                         <a href="http://dch.phil-fak.uni-koeln.de/"
-                        title="Data Center for the Humanities, University of Cologne"
+                        title="Data Center for the Humanities, Universität zu Köln"
                         className="bw"
                         target="_blank"
                         rel="noopener noreferrer">
-                            <img src={logoDCH} alt="DCH"/>
+                            <img src={logoDCH} alt="DCH"/><br/>
+                            Data Center<br/>for the Humanities,<br/>Universität zu Köln
                         </a>
                     </Col>
 
@@ -87,7 +92,8 @@ class Footer extends Component {
                         className="bw"
                         target="_blank"
                         rel="noopener noreferrer">
-                            <img src={logoDFG} alt="Gefördert durch die DFG"/>
+                            <img src={logoDFG} alt="Gefördert durch die DFG"/><br/>
+                            VedaWeb DFG-Projekt
                         </a>
                     </Col>
                 </Row>
@@ -97,7 +103,8 @@ class Footer extends Component {
                 justify="center"
                 align="top"
                 className="bottom-gap"
-                style={{"marginTop": "5rem"}}>
+                style={{"marginTop": "5rem"}}
+                gutter={40}>
 
                     <Col span={3} className="footer-box">
                         <a href="http://c-salt.uni-koeln.de" target="_blank" rel="noopener noreferrer">
@@ -138,9 +145,9 @@ class Footer extends Component {
                 <Row className="top-gap">
                     <Col span={24} className="content-center">
                         <div style={{padding: '3rem 0'}}>
-                            <Link to="/legal">Legal Notice</Link>
-                            <span className="gap-left-big gap-right-big light-grey">&#8213;</span>
-                            <Link to="/privacy">Privacy Notice</Link>
+                            <Link to="/legal"><Icon type="paper-clip"/>Legal Notice</Link>
+                            <span className="gap-left-big gap-right-big"></span>
+                            <Link to="/privacy"><Icon type="paper-clip"/>Privacy Notice</Link>
                         </div>
                     </Col>
                 </Row>
