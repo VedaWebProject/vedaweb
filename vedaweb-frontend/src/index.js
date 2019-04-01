ac6049a70d6c5008af96fe1200bef701f85cdd34
bkis <kiss@mailbox.org>
2019-04-01 11:02:57 +0200

    updated browserizr calls for new version

--- vedaweb-frontend/src/index.js
+++ vedaweb-frontend/src/index.js
@@ -9,15 +9,16 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import Browserizr from 'browserizr';
+const browser = Browserizr.detect();
 
 
 //alert and block if IE/Edge
-if (Browserizr.is_ie || Browserizr.is_edge) {
+if (browser.isIE() || browser.isEdge()) {
     alert("We suspect you are using the Internet Explorer or Microsoft Edge.\nUnfortunately, VedaWeb only works with modern web browsers.\nYou will need to use Firefox, Chrome or something similar instead.");
 } else {
 
     //info for mobile
-    if (Browserizr.is_mobile) {
+    if (browser.isMobile()) {
         alert("We suspect you are using a mobile device.\nVedaWeb is not optimized for mobile devices. We recommend to use a desktop browser instead.");
     }
 
