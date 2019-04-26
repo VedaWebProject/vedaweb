7cefe400a1d5392792ce6ea74b76928144fba23f
bkis <kiss@mailbox.org>
2019-04-26 17:55:10 +0200

    usage stats fix

--- vedaweb-frontend/src/components/utils/usageStats.js
+++ vedaweb-frontend/src/components/utils/usageStats.js
@@ -4,28 +4,25 @@ const usageStats = {
     load: function(acceptedTracking){
         if (!acceptedTracking) return;
         usageStats.track(acceptedTracking);
-
         var _paq = window._paq || [];
-        window._paq = _paq;
-
-        if (window.Matomo) return;
 
         (function() {
-        var u="https://piwik.rrz.uni-koeln.de/piwik/";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '253']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            var u="https://piwik.rrz.uni-koeln.de/piwik/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '253']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
         })();
+
+        window._paq = _paq;
     },
 
     track: function(acceptedTracking, location){
-        if (!acceptedTracking) return;
         var _paq = window._paq || [];
         if (location) _paq.push(['setCustomUrl', usageStats.normalizeLocation(location.pathname)]);
         _paq.push(['enableHeartBeatTimer', 10]);
         _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
+        window._paq = _paq;
     },
 
     normalizeLocation(locationPath){
