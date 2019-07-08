bcc85cb1d14ebe67f415b7cb9b01634ff343832c
bkis <kiss@mailbox.org>
2019-07-08 16:04:06 +0200

    temporarily excluded swagger to fix req mapping

--- vedaweb-backend/src/main/java/de/unikoeln/vedaweb/VedawebApplication.java
+++ vedaweb-backend/src/main/java/de/unikoeln/vedaweb/VedawebApplication.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableSwagger2
+//@EnableSwagger2
 public class VedawebApplication {
 	
 	public static void main(String[] args) {
