dda4fa0d0efb82169dcdf91102c5be90a8fc420c
bkis <kiss@mailbox.org>
2020-03-02 11:12:15 +0100

    added ExternalResource data class

---
+++ vedaweb-backend/src/main/java/de/unikoeln/vedaweb/document/ExternalResource.java
@@ -0,0 +1,52 @@
+package de.unikoeln.vedaweb.document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class ExternalResource {
+	
+	@ApiModelProperty(notes = "Describing label for this set of references")
+	private String label;
+	
+	@ApiModelProperty(notes = "A list of one or more references")
+	private List<String> references;
+	
+	public ExternalResource() {
+		// empty default constructor needed for Spring obj initialization
+	}
+
+	public ExternalResource(String label, String firstReference) {
+		super();
+		this.label = label;
+		this.references = new ArrayList<String>();
+		addReference(firstReference);
+	}
+	
+	public ExternalResource(String label) {
+		this(label, null);
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public List<String> getReferences() {
+		return references;
+	}
+	
+	public void addReference (String reference) {
+		if (reference != null)
+			this.references.add(reference);
+	}
+
+	public void setReferences(List<String> references) {
+		this.references = references;
+	}
+	
+}
