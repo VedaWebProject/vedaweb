365458951ab8e015c64c8f00f9dd02b1488cbcc3
bkis <kiss@mailbox.org>
2020-08-05 12:21:23 +0200

    Code formatting

--- vedaweb-frontend/src/components/widgets/HelpButton.js
+++ vedaweb-frontend/src/components/widgets/HelpButton.js
@@ -618,7 +618,7 @@ const helpTexts = {
         content:
             <div>
                 <p>
-                Müller, Max. 1891. Vedic Hymns. Part 1. Hymns to the Maruts, Rudra, Vâyu and Vâta. Sacred Books of the East. Vol. 32. Oxford: Clarendon Press.
+                    Müller, Max. 1891. Vedic Hymns. Part 1. Hymns to the Maruts, Rudra, Vâyu and Vâta. Sacred Books of the East. Vol. 32. Oxford: Clarendon Press.
                 </p>
             </div>
     },
