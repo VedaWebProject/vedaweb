f959775035c5cb23303d1bce5af430fe0ba1b79d
bkis <kiss@mailbox.org>
2020-11-09 16:57:33 +0100

    Uppercase search mode in query display

--- vedaweb-frontend/src/components/search/queryRepresentation.js
+++ vedaweb-frontend/src/components/search/queryRepresentation.js
@@ -1,9 +1,10 @@
 const queryRepresentation = (queryJSON, ui) =>
-    queryJSON.mode + " search: " +
+    queryJSON.mode.charAt(0).toUpperCase() +
+    queryJSON.mode.slice(1) + " Search: " +
     ( queryJSON.mode === "grammar"
         ? "[" + queryJSON.blocks.map(b =>
-            Object.keys(b).filter(k => k !== 'distance' && b[k] !== undefined && b[k] !== '')
-                .map(k => k + ': ' + b[k]).join(', ')).join('] & [') + "]"
+            Object.keys(b).filter(k => k !== "distance" && b[k] !== undefined && b[k] !== "")
+                .map(k => k + ": " + b[k]).join(", ")).join("] & [") + "]"
         : "\"" + queryJSON.input + "\""
     ) +
     ( queryJSON.mode !== "grammar" &&
