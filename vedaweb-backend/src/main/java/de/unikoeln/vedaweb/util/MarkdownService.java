7495005ceb5b3e52106fdead3efc584469375f7e
bkis <kiss@mailbox.org>
2020-11-17 15:49:37 +0100

    Add markdown table extension

--- vedaweb-backend/src/main/java/de/unikoeln/vedaweb/util/MarkdownService.java
+++ vedaweb-backend/src/main/java/de/unikoeln/vedaweb/util/MarkdownService.java
@@ -1,4 +1,9 @@
 package de.unikoeln.vedaweb.util;
+import java.util.Arrays;
+import java.util.List;
+
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.stereotype.Service;
@@ -16,8 +21,13 @@ public class MarkdownService {
 	private HtmlRenderer renderer;
 	
 	public MarkdownService() {
-		this.parser = Parser.builder().build();
-		this.renderer = HtmlRenderer.builder().build();
+		List<Extension> extensions = Arrays.asList(TablesExtension.create());
+		this.parser = Parser.builder()
+				.extensions(extensions)
+				.build();
+		this.renderer = HtmlRenderer.builder()
+				.extensions(extensions)
+				.build();
 	}
 	
 	/**
