3220b2a81344d870ff601c039ad1f160ad5fe780
bkis <kiss@mailbox.org>
2020-12-15 17:56:57 +0100

    Improve data update script

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -12,10 +12,10 @@ mkdir /tmp/vedaweb/
 wget -q -O /tmp/vedaweb/tei.tar.gz https://github.com/cceh/c-salt_vedaweb_tei/archive/master.tar.gz
 mkdir -p resources/tei
 tar -xf /tmp/vedaweb/tei.tar.gz -C resources/tei --strip 1
-rm /tmp/vedaweb/tei.tar.gz
 
 # download and extract references data
 wget -q -O /tmp/vedaweb/references.tar.gz https://github.com/VedaWebPlatform/vedaweb-data-external/archive/master.tar.gz
 mkdir -p resources/references
 tar -xf /tmp/vedaweb/references.tar.gz -C resources/references --strip 1
-rm /tmp/vedaweb/references.tar.gz
+
+rm -r /tmp/vedaweb
