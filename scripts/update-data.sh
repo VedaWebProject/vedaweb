fdb353a332f35080afdc52609d72b6d4a9da0719
bkis <kiss@mailbox.org>
2022-09-20 17:11:45 +0200

    Update tei data update script

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-#
-#   THIS SCRIPT EXPECTS THE WORKING DIRECTORY TO BE
-#   THE PROJECT DIRECTORY (PARENT OF THIS ONE) !!!!
-#
+if [ -z "$1" ] ; then
+    echo "The path to the target resources directory must be given as the only argument!"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "$1 is not a valid directory!"
+    exit 1
+fi
+
+# temp dir
+vw_temp="/tmp/vedaweb_$EPOCHSECONDS/"
 
 # create temporary directory
-mkdir /tmp/vedaweb/
+mkdir "$vw_temp"
 
 # download and extract TEI data
-wget -q -O /tmp/vedaweb/tei.tar.gz https://github.com/cceh/c-salt_vedaweb_tei/archive/master.tar.gz
-mkdir -p resources/tei
-tar -xf /tmp/vedaweb/tei.tar.gz -C resources/tei --strip 1
+wget -q -O "$vw_temp/tei.tar.gz" https://github.com/cceh/c-salt_vedaweb_tei/archive/master.tar.gz
+mkdir -p "$1/tei"
+tar -xf "$vw_temp/tei.tar.gz" -C "$1/tei" --strip 1
 
 # download and extract references data
-wget -q -O /tmp/vedaweb/references.tar.gz https://github.com/VedaWebPlatform/vedaweb-data-external/archive/master.tar.gz
-mkdir -p resources/references
-tar -xf /tmp/vedaweb/references.tar.gz -C resources/references --strip 1
+wget -q -O "$vw_temp/references.tar.gz" https://github.com/VedaWebPlatform/vedaweb-data-external/archive/master.tar.gz
+mkdir -p "$1/references"
+tar -xf "$vw_temp/references.tar.gz" -C "$1/references" --strip 1
 
-rm -r /tmp/vedaweb
+rm -r "$vw_temp"
