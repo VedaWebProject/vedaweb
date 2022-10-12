ce41544b8523ac1624fd2c953c98afba065a523d
bkis <kiss@mailbox.org>
2022-10-12 14:35:23 +0200

    Adapt privacy policy

--- vedaweb-frontend/src/components/static/PrivacyPolicy.js
+++ vedaweb-frontend/src/components/static/PrivacyPolicy.js
@@ -12,7 +12,7 @@ class ContentView extends Component {
         document.title = "VedaWeb | Privacy Policy";
 
         return (
-            
+
             <Row
             type="flex"
             justify="center"
@@ -22,23 +22,24 @@ class ContentView extends Component {
                     <div className="card">
                         <h1><Icon type="paper-clip"/> Privacy Policy</h1>
                         <p>
-                            Generally, the <a href="https://www.portal.uni-koeln.de/datenschutz.html?L=1" target="_blank" rel="noopener noreferrer">privacy policy of the University of Cologne</a> (also 
+                            Generally, the <a href="https://www.portal.uni-koeln.de/datenschutz.html?L=1" target="_blank" rel="noopener noreferrer">privacy policy of the University of Cologne</a> (also
                             available in <a href="https://www.portal.uni-koeln.de/datenschutz.html?L=0" target="_blank" rel="noopener noreferrer">German</a>)<br/>
-                            applies to this project; with the specific additions stated below:
+                            applies to this project; with the specific additions regarding usage analytics (and a way to opt-out) stated <a href="https://cceh.uni-koeln.de/impressum/" target="_blank" rel="noopener noreferrer">here</a>
+                            and the additions regarding the (entirely local) storage of application settings stated below:
                         </p>
 
                         <h2>Storage of application settings</h2>
                         <p>
                             The client application (or website) of VedaWeb saves part of your application settings in the local storage of your browser.
-                            This ensures that you will find the same settings again when you use the application the next time and won't be presented with 
+                            This ensures that you will find the same settings again when you use the application the next time and won't be presented with
                             any kind of welcome message for first time visitors over and over again.<br/>
-                            The data stored this way is no personal data and will not be stored on 
+                            The data stored this way is no personal data and will not be stored on
                             our servers but only on the device you use to visit this website.
                         </p>
                     </div>
                 </Col>
             </Row>
-            
+
         );
     }
 }
